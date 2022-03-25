a705cfaec38bd6b599998960da9dcaa5f3082365
Maximilian Schebek <schebek@physik.hu-berlin.de>
2022-03-25 13:28:06 +0100

    clean up

--- _ham.py
+++ _ham.py
@@ -1,7 +1,10 @@
 import sys
 import numpy as np
 
-
+# Class implementing the Fourier representation
+# of the 1-dimensional single-particle Hamilton operator.
+# Implementation follows:
+# https://www.physik.hu-berlin.de/de/com/teachingandseminars/previousCPII
 class Hamiltonian:
     def __init__(self, length, n_steps):
         if np.mod(n_steps, 2) != 1:
@@ -25,6 +28,7 @@ class Hamiltonian:
             -self.m, self.m, self.n_steps
         )
         H = np.zeros((self.n_steps, self.n_steps))
+
         # Matrix A with elements A_kl = x_k - x_l
         x_mat = np.outer(self.x_values, np.ones(self.n_steps))
         A_mat = x_mat - np.transpose(x_mat)
@@ -34,6 +38,7 @@ class Hamiltonian:
 
     def diagonalize(self):
         eigvals, eigvecs = np.linalg.eig(self.hamiltonian)
+
         # Sort eigenvalues by energy
         index = np.argsort(eigvals)
         self.eigvecs = eigvecs[:, index]
@@ -41,21 +46,20 @@ class Hamiltonian:
 
     def calc_scatter_phases(self):
         # Keep only scattering solutions with positive energy
-        eigvals_scatter = np.delete(self.eigvals,np.where(self.eigvals<0))
-        eigvecs_scatter=np.delete(self.eigvecs,np.where(self.eigvals<0),axis=1)
+        eigvals_scatter = np.delete(self.eigvals, np.where(self.eigvals < 0))
+        eigvecs_scatter = np.delete(self.eigvecs, np.where(self.eigvals < 0), axis=1)
 
-        #Sort wavefunctions by parity
+        # Sort wavefunctions by parity
         eigvecs_rev = eigvecs_scatter[::-1]
-        ids_neg = np.where(np.all(np.isclose(eigvecs_rev, -eigvecs_scatter, atol=1e-5), axis=0))[0]
-        ids_pos = np.where(np.all(np.isclose(eigvecs_rev, eigvecs_scatter, atol=1e-5), axis=0))[0] 
+        ids_neg = np.where(
+            np.all(np.isclose(eigvecs_rev, -eigvecs_scatter, atol=1e-5), axis=0)
+        )[0]
+        ids_pos = np.where(
+            np.all(np.isclose(eigvecs_rev, eigvecs_scatter, atol=1e-5), axis=0)
+        )[0]
 
-        if len(ids_neg) + len(ids_pos) != len(self.eigvals):
-            print('missing wave function')
-        else:
-            print('all Wavefunctions included')
-
-        # Commpute values corresponding to positive or negative parity 
-        kvals_pos =np.sqrt(2 * eigvals_scatter[ids_pos] + 0j)
+        # Compute values corresponding to positive or negative parity
+        kvals_pos = np.sqrt(2 * eigvals_scatter[ids_pos] + 0j)
         kvals_neg = np.sqrt(2 * eigvals_scatter[ids_neg] + 0j)
         delta_pos = np.real(-1j * np.log(np.exp(-1j * kvals_pos * self.length)) / 2)
         delta_neg = np.real(-1j * np.log(np.exp(-1j * kvals_neg * self.length)) / 2)
